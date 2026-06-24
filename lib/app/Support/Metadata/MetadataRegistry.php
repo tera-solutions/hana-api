6d5153b08c7c83aab613de7f35c97d67dbaa2a97
Duy Nguyen <ngdduy17427@gmail.com>
2026-06-24 13:27:33 +0700

    Update Metadata Registry

--- lib/app/Support/Metadata/MetadataRegistry.php
+++ lib/app/Support/Metadata/MetadataRegistry.php
@@ -8,15 +8,40 @@ use App\Enums\Shared\Gender;
 use App\Enums\Shared\GuardianRelation;
 use App\Modules\CRM\Lead\Enums\LeadStatus;
 use App\Modules\CRM\Parent\Enums\ParentStatus;
+use App\Modules\Education\Assignment\Enums\AssignmentStatus;
+use App\Modules\Education\Assignment\Enums\AssignmentType;
+use App\Modules\Education\Assignment\Enums\SubmissionStatus;
 use App\Modules\Education\Attendance\Enums\AttendanceStatus;
 use App\Modules\Education\ClassRoom\Enums\ClassLearningType;
 use App\Modules\Education\ClassRoom\Enums\ClassStatus;
 use App\Modules\Education\ClassRoom\Enums\ClassStudentStatus;
 use App\Modules\Education\ClassSession\Enums\ClassSessionStatus;
 use App\Modules\Education\Enrollment\Enums\EnrollmentStatus;
+use App\Modules\Education\Evaluation\Enums\EvaluationClassification;
+use App\Modules\Education\Evaluation\Enums\EvaluationPeriod;
+use App\Modules\Education\Evaluation\Enums\EvaluationStatus;
+use App\Modules\Education\Evaluation\Enums\EvaluationType;
+use App\Modules\Education\Evaluation\Enums\EvaluatorType;
+use App\Modules\Education\Exam\Enums\ExamSessionStatus;
+use App\Modules\Education\Exam\Enums\ExamSkill;
+use App\Modules\Education\Exam\Enums\ExamStatus;
+use App\Modules\Education\Exam\Enums\ExamType;
+use App\Modules\Education\Exam\Enums\RegistrationStatus;
+use App\Modules\Education\LeaveRequest\Enums\LeaveReasonType;
+use App\Modules\Education\LeaveRequest\Enums\LeaveRequestType;
+use App\Modules\Education\LeaveRequest\Enums\LeaveStatus;
+use App\Modules\Education\LeaveRequest\Enums\MakeupStatus;
 use App\Modules\Education\Lesson\Enums\LessonStatus;
 use App\Modules\Education\LessonPlan\Enums\LessonPlanStatus;
-use App\Modules\Education\LessonPlanMaterial\Enums\MaterialType;
+use App\Modules\Education\LessonPlanMaterial\Enums\MaterialType as LessonPlanMaterialType;
+use App\Modules\Education\Material\Enums\MaterialAccessType;
+use App\Modules\Education\Material\Enums\MaterialEntityType;
+use App\Modules\Education\Material\Enums\MaterialStatus;
+use App\Modules\Education\Material\Enums\MaterialType;
+use App\Modules\Education\Question\Enums\QuestionDifficulty;
+use App\Modules\Education\Question\Enums\QuestionSkill;
+use App\Modules\Education\Question\Enums\QuestionStatus;
+use App\Modules\Education\Question\Enums\QuestionType;
 use App\Modules\Education\Room\Enums\RoomStatus;
 use App\Modules\Education\Room\Enums\RoomType;
 use App\Modules\Education\Student\Enums\StudentStatus;
@@ -29,8 +54,19 @@ use App\Modules\Finance\Invoice\Enums\PartnerType;
 use App\Modules\Finance\Payment\Enums\PaymentDirection;
 use App\Modules\Finance\Payment\Enums\PaymentStatus;
 use App\Modules\Finance\Payment\Enums\PaymentType;
+use App\Modules\Finance\Promotion\Enums\DiscountType;
+use App\Modules\Finance\Promotion\Enums\PromotionStatus;
+use App\Modules\Finance\Promotion\Enums\PromotionType;
+use App\Modules\Finance\Promotion\Enums\ReferralStatus;
+use App\Modules\Finance\Promotion\Enums\VoucherStatus;
+use App\Modules\Finance\Wallet\Enums\WalletAdjustmentType;
+use App\Modules\Finance\Wallet\Enums\WalletOwnerType;
+use App\Modules\Finance\Wallet\Enums\WalletStatus;
+use App\Modules\Finance\Wallet\Enums\WalletTransactionType;
 use App\Modules\HR\Teacher\Enums\TeacherStatus;
 use App\Modules\HR\Teacher\Enums\TeacherType;
+use App\Modules\System\ActivityLog\Enums\ActivityAction;
+use App\Modules\System\ActivityLog\Enums\ActivityModule;
 use App\Modules\System\Branch\Enums\BranchStatus;
 use App\Modules\System\Business\Enums\BusinessStatus;
 use App\Modules\System\User\Enums\UserStatus;
@@ -59,6 +95,8 @@ class MetadataRegistry
                 'user_status' => UserStatus::options(),
                 'business_status' => BusinessStatus::options(),
                 'branch_status' => BranchStatus::options(),
+                'activity_action' => ActivityAction::options(),
+                'activity_module' => ActivityModule::options(),
             ],
             'crm' => [
                 'lead_status' => LeadStatus::options(),
@@ -75,8 +113,38 @@ class MetadataRegistry
                 'room_status' => RoomStatus::options(),
                 'room_type' => RoomType::options(),
                 'lesson_plan_status' => LessonPlanStatus::options(),
-                'material_type' => MaterialType::options(),
                 'lesson_status' => LessonStatus::options(),
+                'evaluation_type' => EvaluationType::options(),
+                'evaluator_type' => EvaluatorType::options(),
+                'evaluation_period' => EvaluationPeriod::options(),
+                'evaluation_classification' => EvaluationClassification::options(),
+                'evaluation_status' => EvaluationStatus::options(),
+                'lesson_plan_material_type' => LessonPlanMaterialType::options(),
+
+                'material_type' => MaterialType::options(),
+                'material_status' => MaterialStatus::options(),
+                'material_access_type' => MaterialAccessType::options(),
+                'material_entity_type' => MaterialEntityType::options(),
+
+                'assignment_status' => AssignmentStatus::options(),
+                'assignment_type' => AssignmentType::options(),
+                'assignment_submission_status' => SubmissionStatus::options(),
+
+                'question_status' => QuestionStatus::options(),
+                'question_type' => QuestionType::options(),
+                'question_skill' => QuestionSkill::options(),
+                'question_difficulty' => QuestionDifficulty::options(),
+
+                'exam_status' => ExamStatus::options(),
+                'exam_type' => ExamType::options(),
+                'exam_skill' => ExamSkill::options(),
+                'exam_session_status' => ExamSessionStatus::options(),
+                'exam_registration_status' => RegistrationStatus::options(),
+
+                'leave_status' => LeaveStatus::options(),
+                'leave_request_type' => LeaveRequestType::options(),
+                'leave_reason_type' => LeaveReasonType::options(),
+                'makeup_status' => MakeupStatus::options(),
             ],
             'hr' => [
                 'teacher_status' => TeacherStatus::options(),
@@ -94,6 +162,16 @@ class MetadataRegistry
                 'debt_status' => DebtStatus::options(),
                 'adjustment_type' => AdjustmentType::options(),
                 'refund_status' => RefundStatus::options(),
+                'wallet_owner_type' => WalletOwnerType::options(),
+                'wallet_status' => WalletStatus::options(),
+                'wallet_transaction_type' => WalletTransactionType::options(),
+                'wallet_adjustment_type' => WalletAdjustmentType::options(),
+
+                'promotion_status' => PromotionStatus::options(),
+                'promotion_type' => PromotionType::options(),
+                'discount_type' => DiscountType::options(),
+                'voucher_status' => VoucherStatus::options(),
+                'referral_status' => ReferralStatus::options(),
             ],
         ];
     }
